4915b8ef35de66f1ff264a5eaf89af903b32750b
Kevin Gong <gyhkevin@outlook.com>
2017-08-08 16:56:32 +0800

    init pos

---
+++ src/components/page/Pos.vue
@@ -0,0 +1,17 @@
+<<template>
+    <div class="pos">
+        <i class="icon iconfont icon-hanbao pos"></i>
+      Hello Pos Demo!
+    </div>
+</template>
+<script>
+export default {
+  name: 'Pos'
+}
+</script>
+<style scoped>
+.pos{
+    font-size: 48px;
+}
+</style>
+
